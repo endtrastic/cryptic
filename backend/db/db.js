baf9c8bb95bbf1bd5b7dd2c161d6f624eb673823
endtrastic <a.tuvikene@voco.ee>
2025-10-21 19:56:06 +0300

    Trying to remove config file, because I actually need to enter important data there

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,21 @@
 require('dotenv').config(); 
 
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,     
+//   process.env.DB_USER,     
+//   process.env.DB_PASSWORD, 
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',      
+//     logging: false,        
+//   }
+// );
+
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -8,10 +24,17 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',      
+    dialect: 'postgresql',      
     logging: false,        
   }
 );
 
 
 module.exports = sequelize;
+
+// import postgres from 'postgres'
+
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
+
+// export default sql
